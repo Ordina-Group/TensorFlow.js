f979dd31e6918c62e834969984a048083cff56b9
Kevin Van den Abeele <kevin.vandenabeele@gmail.com>
2019-04-30 13:13:11 +0200

    Added more styles

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let inputImg;
 let statusMsg;
 let transferBtn;
-let style1;
-let style2;
+let style1, style2, style3, style4, style5, style6, style7, style8, style9;
 
 let fileInput;
 let dropContainer;
@@ -21,9 +20,11 @@ function setup() {
     fileInput.disabled = true;
     dropContainer.disabled = true;
 
+    inputImg = document.getElementById('sourceImg');
+
     fileInput.addEventListener('change', () => {
         if(fileInput.files.length === 1) {
-            inputImg = createImg(URL.createObjectURL(fileInput.files[0]));
+            inputImg.src = URL.createObjectURL(fileInput.files[0]);
         } else {
             statusMsg.html('Too many files selected, only select one file!');
         }
@@ -36,18 +37,28 @@ function setup() {
             fileInput.files = evt.dataTransfer.files;
             evt.preventDefault();
 
-            inputImg = createImg(URL.createObjectURL(fileInput.files[0]));
+            inputImg.src = URL.createObjectURL(fileInput.files[0]);
         } else {
             statusMsg.html('Too many files dropped, only drag in one file!');
         }
     };
 
-    style1 = ml5.styleTransfer('models/wave', modelLoaded);
-    style2 = ml5.styleTransfer('models/udnie', modelLoaded);
+    style1 = ml5.styleTransfer('models/fuchun', modelLoaded);
+    style2 = ml5.styleTransfer('models/la_muse', modelLoaded);
+    style3 = ml5.styleTransfer('models/mathura', modelLoaded);
+    style4 = ml5.styleTransfer('models/rain_princess', modelLoaded);
+    style5 = ml5.styleTransfer('models/scream', modelLoaded);
+    style6 = ml5.styleTransfer('models/udnie', modelLoaded);
+    style7 = ml5.styleTransfer('models/wave', modelLoaded);
+    style8 = ml5.styleTransfer('models/wreck', modelLoaded);
+    style9 = ml5.styleTransfer('models/zhangdaqian', modelLoaded);
 }
 
 function modelLoaded() {
-    if (style1.ready && style2.ready) {
+    if (style1.ready && style2.ready && style3.ready &&
+        style4.ready && style5.ready && style6.ready &&
+        style7.ready && style8.ready && style9.ready) {
+
         statusMsg.html('Ready!');
 
         fileInput.disabled = false;
@@ -63,6 +74,27 @@ function transferImages() {
     });
     style2.transfer(inputImg, function (err, result) {
         createImg(result.src).parent('styleB');
+    });
+    style3.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleC');
+    });
+    style4.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleD');
+    });
+    style5.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleE');
+    });
+    style6.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleF');
+    });
+    style7.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleG');
+    });
+    style8.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleH');
+    });
+    style9.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleI');
     });
 
     statusMsg.html('Done!');
